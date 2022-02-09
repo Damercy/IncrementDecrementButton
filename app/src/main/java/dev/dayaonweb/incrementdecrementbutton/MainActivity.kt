b0bab7404c39452eadc38ec010af7a08258b630d
Damercy <adhikaridayamoy@gmail.com>
2022-02-09 16:58:44 +0530

    add base button complete

--- app/src/main/java/dev/dayaonweb/incrementdecrementbutton/MainActivity.kt
+++ app/src/main/java/dev/dayaonweb/incrementdecrementbutton/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn: IncrementDecrementButton = findViewById(R.id.btn_inc_dec)
     }
 }
--- incrementdecrementbutton/src/main/java/dev/dayaonweb/incrementdecrementbutton/IncrementDecrementButton.kt
+++ incrementdecrementbutton/src/main/java/dev/dayaonweb/incrementdecrementbutton/IncrementDecrementButton.kt
@@ -3,25 +3,26 @@ package dev.dayaonweb.incrementdecrementbutton
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.LinearLayout
 import androidx.annotation.FontRes
-import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textview.MaterialTextView
 
 class IncrementDecrementButton @JvmOverloads constructor(
     context: Context,
-    attrs: AttributeSet,
-    defStyleAttr: Int,
-    defSStyleRes: Int
-) : LinearLayout(context, attrs, defStyleAttr, defSStyleRes) {
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0,
+    defStyleRes: Int = 0
+) : LinearLayout(context, attrs, defStyle, defStyleRes) {
 
     private var fontFamily: Int
     private var fontSize: Int
     private var decrementText: String
     private var incrementText: String
     private var middleText: String
+    private var value = 0
 
     private lateinit var btnIncrement: MaterialButton
     private lateinit var btnDecrement: MaterialButton
@@ -32,11 +33,11 @@ class IncrementDecrementButton @JvmOverloads constructor(
         context.theme.obtainStyledAttributes(
             attrs,
             R.styleable.IncrementDecrementButton,
-            defStyleAttr,
-            defSStyleRes
+            defStyle,
+            defStyleRes
         ).apply {
-            fontFamily = getResourceId(R.styleable.IncrementDecrementButton_fontFamily, -1)
-            fontSize = getInt(R.styleable.IncrementDecrementButton_fontSize, DEFAULT_FONT_SIZE)
+            fontFamily = getResourceId(R.styleable.IncrementDecrementButton_fontFamilyRes, -1)
+            fontSize = getInt(R.styleable.IncrementDecrementButton_fontSizeRes, DEFAULT_FONT_SIZE)
             decrementText = getString(R.styleable.IncrementDecrementButton_decrementText)
                 ?: DEFAULT_DECREMENT_TEXT
             incrementText = getString(R.styleable.IncrementDecrementButton_incrementText)
@@ -49,6 +50,7 @@ class IncrementDecrementButton @JvmOverloads constructor(
     }
 
 
+    // public setters
     fun setFontFamily(@FontRes fontRes: Int) {
         if (fontRes == -1) return
         val font = ResourcesCompat.getFont(context, fontRes)
@@ -58,16 +60,75 @@ class IncrementDecrementButton @JvmOverloads constructor(
         invalidateLayout()
     }
 
+    fun setIncrementButtonText(text: String) {
+        incrementText = text
+        setResourceText(btnIncrement, incrementText)
+    }
+
+    fun setDecrementButtonText(text: String) {
+        decrementText = text
+        setResourceText(btnDecrement, decrementText)
+    }
+
+    fun setMiddleText(text: String) {
+        middleText = text
+        setResourceText(btnText, middleText)
+    }
+
+    // public getters
+    fun getCurrentValue() = value
+
+
     private fun initializeIncDecButton() {
         btnIncrement = findViewById(R.id.btn_increment)
         btnDecrement = findViewById(R.id.btn_decrement)
         btnText = findViewById(R.id.btn_text)
         setFontFamily(fontFamily)
+        setIncrementButtonText(incrementText)
+        setDecrementButtonText(decrementText)
+        setMiddleText(middleText)
+        attachListeners()
+    }
+
+    private fun attachListeners() {
+        btnIncrement.setOnClickListener {
+            value++
+            onIncrement()
+        }
+        btnDecrement.setOnClickListener {
+            value--
+            onDecrementRangeCheck()
+        }
+        btnText.setOnClickListener {
+            value++
+            onIncrement()
+        }
+    }
+
+    private fun onIncrement() {
+        if (value > 0)
+            setResourceText(btnText, value.toString())
+    }
+
+    private fun onDecrementRangeCheck() {
+        if (value <= 0) {
+            setResourceText(btnText, middleText)
+            value = 0
+        } else
+            setResourceText(btnText, value.toString())
     }
 
     private fun invalidateLayout() {
         invalidate()
         requestLayout()
+    }
+
+    private fun setResourceText(view: View, text: String) {
+        when (view) {
+            is MaterialButton -> view.text = text
+            is MaterialTextView -> view.text = text
+        }
+        invalidateLayout()
     }
 
 
