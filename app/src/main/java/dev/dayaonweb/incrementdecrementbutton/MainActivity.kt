a12161da0be7c5985a073eb7e3e5f5f4bd10500f
Damercy <adhikaridayamoy@gmail.com>
2022-04-24 01:49:10 +0530

    update versions & add composable button v1

--- app/src/main/java/dev/dayaonweb/incrementdecrementbutton/MainActivity.kt
+++ app/src/main/java/dev/dayaonweb/incrementdecrementbutton/MainActivity.kt
@@ -2,10 +2,20 @@ package dev.dayaonweb.incrementdecrementbutton
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.platform.ComposeView
+import dev.dayaonweb.incrementdecrementbutton.composable.IncrementDecrementButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var composeView: ComposeView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        composeView = findViewById(R.id.compose_view)
+        composeView.setContent {
+            MaterialTheme {
+                IncrementDecrementButton()
+            }
+        }
     }
 }
--- incrementdecrementbutton/src/main/java/dev/dayaonweb/incrementdecrementbutton/composable/IncrementDecrementButton.kt
+++ incrementdecrementbutton/src/main/java/dev/dayaonweb/incrementdecrementbutton/composable/IncrementDecrementButton.kt
@@ -1,14 +1,21 @@
 package dev.dayaonweb.incrementdecrementbutton.composable
 
+import android.annotation.SuppressLint
 import android.graphics.Typeface
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.contentColorFor
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.text.toUpperCase
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -17,9 +24,10 @@ import dev.dayaonweb.incrementdecrementbutton.animations.AnimationType
 
 @Composable
 fun IncrementDecrementButton(
-    decrementComposable: @Composable () -> Unit = { DefaultDecrementComposable() },
-    incrementComposable: @Composable () -> Unit = { DefaultIncrementComposable() },
-    middleComposable: @Composable () -> Unit = { DefaultMiddleComposable() },
+    modifier: Modifier = Modifier,
+    decrementComposable: @Composable (Modifier) -> Unit = { DefaultDecrementComposable(it) },
+    incrementComposable: @Composable (Modifier) -> Unit = { DefaultIncrementComposable(it) },
+    middleComposable: @Composable (Modifier) -> Unit = { DefaultMiddleComposable(it) },
     fontFamily: FontFamily = FontFamily(typeface = Typeface.DEFAULT),
     fontSize: TextUnit = 16.0.sp,
     cornerShape: Shape = RoundedCornerShape(8.dp),
@@ -27,23 +35,57 @@ fun IncrementDecrementButton(
     animationDuration: Double = 500.0,
     backgroundColor: Color = MaterialTheme.colors.background,
     contentColor: Color = MaterialTheme.colors.contentColorFor(backgroundColor),
-    borderStroke: BorderStroke = BorderStroke(0.dp, Color.White)
+    borderStroke: BorderStroke = BorderStroke(0.dp, Color.White),
 ) {
-
+    val buttonModifier = modifier
+        .background(
+            color = backgroundColor,
+            shape = cornerShape
+        )
+        .border(
+            border = borderStroke,
+            shape = cornerShape
+        )
+    Row{
+        decrementComposable(buttonModifier)
+        middleComposable(buttonModifier)
+        incrementComposable(buttonModifier)
+    }
 }
 
 
 @Composable
-fun DefaultDecrementComposable() {
-
+fun DefaultDecrementComposable(
+    modifier: Modifier = Modifier
+) {
+    TextButton(onClick = { /*TODO*/ }, modifier = modifier) {
+        Text(text = "-")
+    }
 }
 
 @Composable
-fun DefaultIncrementComposable() {
-
+fun DefaultIncrementComposable(
+    modifier: Modifier = Modifier
+) {
+    TextButton(onClick = { /*TODO*/ }, modifier = modifier) {
+        Text(text = "+")
+    }
 }
 
 @Composable
-fun DefaultMiddleComposable() {
+fun DefaultMiddleComposable(
+    modifier: Modifier = Modifier
+) {
+    TextButton(onClick = { /*TODO*/ }, modifier = modifier) {
+        Text(text = "Add".toUpperCase(Locale.current))
+    }
+}
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Preview
+@Composable
+fun IncrementDecrementButtonPreview() {
+    Scaffold {
+        IncrementDecrementButton()
+    }
 }
